4f7fd6b3b457e4ec48ed30f5f3e63fc1a15c0cae
Jason Aviles <sandman2k18@outlook.com>
2019-09-02 07:46:08 -0400

    fixing cors issue

--- server.js
+++ server.js
@@ -17,9 +17,20 @@ server.use(helmet());
 
 
 
-server.all('*', function(req, res, next) {
+
+
+
+
+
+server.use(cors());
+server.options("*", cors());
+
+server.use(morgan("combined"));
+server.use(express.json());
+
+server.use('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
   if (req.method == 'OPTIONS') {
@@ -28,46 +39,6 @@ server.all('*', function(req, res, next) {
       next();
   }
 });
-
-
-server.use(function(req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  next();
-});
-server.use(cors());
-server.options("*", cors());
-
-server.use(morgan("combined"));
-server.use(express.json());
 
 server.use("/detail", detailPublic);
 server.use("/public", publicApi);
