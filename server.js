2916acb8cd9147848c55b5c8ca5db437a0c87282
Jason Aviles <sandman2k18@outlook.com>
2019-09-02 07:22:35 -0400

    fixing cors issue

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ server.use(helmet());
 
 server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); res.header("optionsSuccessStatus",200) 
+  res.header("Access-Control-Allow-Headers", "*");res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); res.header("optionsSuccessStatus",200) 
   next();
 });
 server.use(cors());
