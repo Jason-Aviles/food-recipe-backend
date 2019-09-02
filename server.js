068da742d931054c405092d2b1a323f9e08485f1
Jason Aviles <sandman2k18@outlook.com>
2019-09-02 08:06:02 -0400

    fixing cors issue

--- server.js
+++ server.js
@@ -21,20 +21,12 @@ server.use(helmet());
 
 
 
-
-server.use(cors());
+app.use(cors({origin:"http://localhost:3006"}));
 server.options("*", cors());
 
 server.use(morgan("combined"));
 server.use(express.json());
 
-server.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'appid, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  next();
-});
 
 
 server.use("/detail", detailPublic);
