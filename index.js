ff17e16321901f398800d2e525cedefa40b509f3
Jason Aviles <sandman2k18@outlook.com>
2019-09-02 06:47:03 -0400

    fixing cors issue

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const server = require('./server')
 const cors = require('cors')
 require('dotenv').config()
 const PORT = process.env.PORT  || 5000;
-
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 server.use(cors());
 server.options('*', cors());
 
