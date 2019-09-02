ec8c22bf4e7db0948d96e6e8be08dc7167bb7203
Jason Aviles <sandman2k18@outlook.com>
2019-09-02 06:34:29 -0400

    fixing cors issue

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require('cors')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+
+
+
 const authUser = require("./Auth/authRouter");
 const authApi = require("./FoodieAuthApi/authRouter");
 const publicApi = require("./FoodiePublicApi/publicRouter");
@@ -9,10 +15,30 @@ const authMiddleWare =require('./tokenMiddleWare/authenticate')
 const server = express();
   
 
-server.options('*', cors())
+
+var allowCrossDomain = function(req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', '*');
+  
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+  
+  next();
+  
+  };
+  
+  app.use(allowCrossDomain);
+
+server.use(bodyParser.json());
+
 
 server.use(helmet());
 server.use(cors());
+
+server.options('*', cors())
+server.use(morgan('combined'));
 server.use(express.json());
 
 server.use("/detail", detailPublic);
