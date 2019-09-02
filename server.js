d9b969f1288ffdc7a556a1870ca668a2c52e08a6
Jason Aviles <sandman2k18@outlook.com>
2019-09-02 06:38:11 -0400

    fixing cors issue

--- server.js
+++ server.js
@@ -1,50 +1,40 @@
 const express = require("express");
 const helmet = require("helmet");
-const cors = require('cors')
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
-
-
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const authUser = require("./Auth/authRouter");
 const authApi = require("./FoodieAuthApi/authRouter");
 const publicApi = require("./FoodiePublicApi/publicRouter");
-const detailPublic = require("./FoodiePublicApi/detailRouter")
-const authMiddleWare =require('./tokenMiddleWare/authenticate')
+const detailPublic = require("./FoodiePublicApi/detailRouter");
+const authMiddleWare = require("./tokenMiddleWare/authenticate");
 const server = express();
-  
-
 
 var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3006/");
 
-  res.header('Access-Control-Allow-Origin', '*');
-  
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  
-  
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
   next();
-  
-  };
-  
-  app.use(allowCrossDomain);
+};
+
+app.use(allowCrossDomain);
 
 server.use(bodyParser.json());
-
 
 server.use(helmet());
 server.use(cors());
 
-server.options('*', cors())
-server.use(morgan('combined'));
+server.options("*", cors());
+server.use(morgan("combined"));
 server.use(express.json());
 
 server.use("/detail", detailPublic);
- server.use("/public", publicApi);
+server.use("/public", publicApi);
 server.use("/auth", authUser);
-server.use("/auth/api",authMiddleWare, authApi);
-
+server.use("/auth/api", authMiddleWare, authApi);
 
 module.exports = server;
